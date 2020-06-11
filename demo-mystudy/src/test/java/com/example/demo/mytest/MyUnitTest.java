829f76b67e55156592aeba5f08eed8171dcfe42e
kk <2689137906@qq.com>
2020-06-12 01:30:32 +0800

    添加对个人数据的一些过滤,避免junit中一些个人数据泄露

--- demo-mystudy/src/test/java/com/example/demo/mytest/MyUnitTest.java
+++ demo-mystudy/src/test/java/com/example/demo/mytest/MyUnitTest.java
@@ -28,65 +28,9 @@ public class MyUnitTest {
     @Autowired
     private StudentMapper studentMapper;
 
-    @Autowired
-    private AccountNumberMapper accountNumberMapper;
-
-
     @Test
     public void test() {
         System.out.println(studentMapper.selectByNameTest("student", "test12"));
     }
 
-    @Test
-    public void insertAccountNumber() {
-        AccountNumber accountNumber = getAccopuntNumber();
-        System.out.println(accountNumber.getUserPwd());
-        System.out.println(accountNumber.getUserPwd().length());
-        System.out.println(accountNumber);
-        accountNumberMapper.insertSelective(accountNumber);
-    }
-
-    @Test
-    public void findAccountNumber() {
-        //List<AccountNumber> accountNumbers = accountNumberMapper.findBySystematicName("华");
-        List<AccountNumber> accountNumbers = accountNumberMapper.findAll();
-        for (AccountNumber accountNumber : accountNumbers) {
-            System.out.println(accountNumber.toString());
-            System.out.println("账号 : " + accountNumber.getAccountNumber()
-                    + " ;用户名 : " + accountNumber.getUserName()
-                    + " ;密码 : " + EncryptUtils.decryptRSAByPriKey(accountNumber.getUserPwd(), EncryptUtils.getPrivateKey(EncryptUtils.RSA)));
-        }
-    }
-
-    @Test
-    public void updateAccountNumberPwd() {
-        Integer id = 0;
-        AccountNumber accountNumber = accountNumberMapper.selectByPrimaryKey(id);
-        accountNumber.setUserPwd(EncryptUtils.encryptRSAByPubKey("xxx", EncryptUtils.getPublicKey(EncryptUtils.RSA)));
-        accountNumberMapper.updateByPrimaryKeySelective(accountNumber);
-        accountNumber = accountNumberMapper.selectByPrimaryKey(id);
-        System.out.println(accountNumber.toString());
-        System.out.println("账号 : " + accountNumber.getAccountNumber()
-                    + " ;用户名 : " + accountNumber.getUserName()
-                    + " ;密码 : " + EncryptUtils.decryptRSAByPriKey(accountNumber.getUserPwd(), EncryptUtils.getPrivateKey(EncryptUtils.RSA)));
-
-    }
-
-    /**
-     * 获取 AccopuntNumber
-     * @return AccopuntNumber
-     */
-    private AccountNumber getAccopuntNumber() {
-        AccountNumber accountNumber = new AccountNumber();
-        accountNumber.setSystematicName("xxx");
-        accountNumber.setAccountNumber("xxx");
-        accountNumber.setUserName("xxx");
-        accountNumber.setUserPwd(EncryptUtils.encryptRSAByPubKey("xxx", EncryptUtils.getPublicKey(EncryptUtils.RSA)));
-        //accountNumber.setEmail("xxx");
-        accountNumber.setRemarks("xxx");
-        accountNumber.setCreateTime(LocalDateTime.now());
-        accountNumber.setUpdateTime(LocalDateTime.now());
-        accountNumber.setIsValid(1);
-        return accountNumber;
-    }
 }
