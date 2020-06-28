ee2a0e88171d5993fbdd9e8e5d1134a488be23c5
willie <15116251140@163.com>
2020-06-28 11:44:54 +0800

    自定义注解 测试 --v2

--- demo-mystudy/src/test/java/com/example/demo/mytest/AnnotationTest.java
+++ demo-mystudy/src/test/java/com/example/demo/mytest/AnnotationTest.java
@@ -15,8 +15,8 @@ import java.lang.reflect.Field;
 import java.util.Arrays;
 
 /**
- * @ClassName: Annotation
- * @Description:
+ * @ClassName: AnnotationTest
+ * @Description:    自定义注解 测试
  * @Author: kk
  * @version: 1.0.0
  * @Date: 2020/06/26 04:30
@@ -29,7 +29,7 @@ public class AnnotationTest {
      /**
      * 日志(log4j)
      */
-    private static Logger logger = LogManager.getLogger(AnnotationTest.class);
+    private static final Logger logger = LogManager.getLogger(AnnotationTest.class);
 
     @ResourceAnnotation("resourceAnnotationField")
     @RuntimeAnnotation(name = "annotationTest", number = 12, strings = {"stringTest1", "stringTest2"})
@@ -44,6 +44,9 @@ public class AnnotationTest {
         Field field = annotationTest.getDeclaredField("fieldTest");
         // 设置 私有 属性 可访问
         field.setAccessible(true);
+        System.out.println(field);
+        System.out.println(field.getName());
+        System.out.println(field.get(annotationTest.getDeclaredConstructor().newInstance()));
         // 获取该元素上指定类型的注解
         //只有当注解的保持力处于运行阶段，即使用@Retention(RetentionPolicy.RUNTIME)修饰注解时，才能在JVM运行时，检测到注解，并进行一系列特殊操作。
         RuntimeAnnotation runtimeAnnotation = field.getAnnotation(RuntimeAnnotation.class);
